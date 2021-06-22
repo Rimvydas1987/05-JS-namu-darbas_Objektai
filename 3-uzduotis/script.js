e8f2d431dabc9d831b43716be1e648170826acd0
Rimvydas Šimkus <84323090+Rimvydas1987@users.noreply.github.com>
2021-06-22 09:53:36 +0300

    3 Uzduotis

--- 3-uzduotis/script.js
+++ 3-uzduotis/script.js
@@ -6,10 +6,24 @@ document.getElementById("patvirtinta").addEventListener("click", function() {
     sk2 = parseFloat(document.getElementById("sk2").value);
 
     const kvadratas = {
-        sk1: sk1,
-        sk2: sk2,
-        plotas: function() {
-            return this.sk1 * this.sk2;
+        
+        plotas: function(a, b) {
+            return a * b;
+            
+        },
+        perimetras: function(a, b){
+            return 2 * a + 2 * b;
+        },
+        istrizaine: function(a, b) {
+            return Math.sqrt(a * a + b * b);
+        },
+        rezultatas: function(a, b) {
+            document.getElementById("plotas").innerHTML = "Plotas: " + this.plotas(a, b);
+            document.getElementById("perimetras").innerHTML = "Perimetras: " + this.perimetras(a, b);
+            document.getElementById("istrizaine").innerHTML = "Įstrižainė: " + this.istrizaine(a, b);
         },
     };
+
+kvadratas.rezultatas(sk1, sk2);
+
 });
